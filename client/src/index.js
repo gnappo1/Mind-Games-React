f55f68fd265c313ebb8eba9517575e11d02f6e45
gnappo1 <gnappo.1@live.it>
2018-02-12 17:19:28 -0800

    Created containers folder inside ./src. Moved App container and relative style to this folder. Updated index.js with components and middlewares from react and redux. Created store using createStore and passed it down to App using the Provider component. RootReducer still needs to be defined.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
