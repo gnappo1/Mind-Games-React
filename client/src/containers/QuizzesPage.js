24550a52804476861ed3305a1043de49a96b029f
gnappo1 <gnappo.1@live.it>
2018-02-14 00:03:23 -0800

    Updated QuizzesPage creating a componentDidMount and mapStateToProps. I'm importing getQuizzes, which still needs to be defined.

--- client/src/containers/QuizzesPage.js
+++ client/src/containers/QuizzesPage.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {getQuizzes} from '../actions/quizzes';
 
 class QuizzesPage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        windowWidth: window.innerWidth,
+        windowHeight: window.innerHeight,
+        quizzes: []
+    };
+  }
+
+  componentDidMount() {
+    this.props.getQuizzes();
+  }
+
   render() {
+    const {match, quizzes} = this.props
     return (
       <div className="quizzesPage">
         <h1>QUIZZES</h1>
+        <div className="quizzes-div">
+          <QuizzesList quizzes={quizzes} />
+          <Switch>
+            <Route path={`${match.url}/:quizzId`} component={QuizCard} />          
+          </Switch>
+        </div>
       </div>
     )
   }
 }
 
-export default QuizzesPage;
+const mapStateToProps = (state) => {
+  return {quizzes: state.quizzes}
+}
+
+export default connect(mapStateToProps, {getQuizzes})(QuizzesPage);
