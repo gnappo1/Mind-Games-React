e09f84839a8e648145e4239881c593585803ed77
gnappo1 <gnappo.1@live.it>
2018-03-06 04:04:40 -0800

    Styled and edited Home Component.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 const Home = () =>
-
-  <div className="home">
-    <p>Welcome to MPGames</p>
-    <ul> Pick the quiz that you like. </ul>
-    <ul> Make sure you are focused and keep paper and pen close. </ul>
-    <ul> Questions have a timer. Once it starts, it will stop only when you answer the question correctly. </ul>
-    <ul> Each incorrect attemp will decrease your total score of 1 point. </ul>
+  <div className="grid-container">
+    <div id="cool-background">
+      <img src="https://image.freepik.com/free-vector/pack-of-hand-drawn-test-elements_23-2147596296.jpg" alt="background" />
+    </div>
+    <div className="wlcome-text">
+      <h1>Welcome to MPGames</h1><br/>
+      <p> Pick the quiz that you like. </p>
+      <p> Make sure you are focused and keep paper and pen close. </p>
+      <p> Questions have a timer. Once it starts, it will stop only when you answer the question correctly. </p>
+      <p> Each incorrect attemp will decrease your total score of 1 point. </p>
+    </div>
   </div>
 
 export default Home
