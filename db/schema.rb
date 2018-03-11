b82181ea3112c872b6788229d9c1e7ab5e13b4d0
gnappo1 <gnappo.1@live.it>
2018-03-11 00:49:24 -0800

    Set up postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180308140854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "questions", force: :cascade do |t|
     t.string "text"
     t.boolean "completed", default: false
