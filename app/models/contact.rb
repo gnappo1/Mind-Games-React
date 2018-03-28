9fc3532f61f3792e63887cb07eba20ced658f293
gnappo1 <gnappo.1@live.it>
2018-03-28 13:25:58 -0700

    Changes inheritance for Contact model having commented out and rewritten validations.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,13 @@
-class Contact < MailForm::Base
+class Contact < ApplicationRecord
   attribute :name, :validate => true
   attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
 
- def headers
-   {
-   :subject => 'Contact Inquiry',
-   :to => ENV['GMAIL_USERNAME'],
-   :from => %('#{name}' <#{email}>)
-   }
- end
+ # def headers
+ #   {
+ #   :subject => 'Contact Inquiry',
+ #   :to => ENV['GMAIL_USERNAME'],
+ #   :from => %('#{name}' <#{email}>)
+ #   }
+ # end
 end
